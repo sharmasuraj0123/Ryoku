2055c8c650a7081464039a615f8514dad2c031ca
Suraj Sharma <sharmasuraj0123@gmail.com>
2017-11-27 23:55:35 -0500

    Added JDBC connection

---
+++ src/MySQLConnUtils.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnUtils{
+
+
+    // Connect to MySQL
+    public static Connection getMySQLConnection() throws SQLException,
+            ClassNotFoundException {
+        String hostName = "localhost";
+
+        String dbName = "CSE305_RYOKU";
+        String userName = "root";
+        String password = "";
+
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException,
+            ClassNotFoundException {
+        // Declare the class Driver for MySQL DB
+        // This is necessary with Java 5 (or older)
+        // Java6 (or newer) automatically find the appropriate driver.
+        // If you use Java> 5, then this line is not needed.
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+        Connection conn = DriverManager.getConnection(connectionURL, userName,
+                password);
+        return conn;
+    }
+}
